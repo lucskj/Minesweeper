f5b6cf065973668a52d02c1ea02c374b4ee36540
lucskj <lucaskreutzerdejesus@gmail.com>
2022-09-20 19:21:05 -0300

    tentando estruturar o print da matriz

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+void generateBombs(char[16][16]);
+void checkSurroundings(char);
+int gameLoss(char);
+void printGameBoard(char[16][16]);
 
 int main(int argc, char const *argv[]) {
     srand(time(NULL));
-    char gameBoard[16][16];     // Tamanho de um jogo de dificuldade média (40 bombas).
+    char gameBoard[16][16] = {};     // Tamanho de um jogo de dificuldade média (40 bombas).
     generateBombs(gameBoard);
+
+    printGameBoard(gameBoard);
 
     char posicao = gameBoard[0][1];
     checkSurroundings(posicao);
@@ -13,14 +21,13 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-void generateBombs(char *gameBoard[]) {
-    
+void generateBombs(char gameBoard[16][16]) {
     for(int i=0; i<40;i++) {
         int randomLine = rand() % 17;
         int randomColumn = rand() % 17;
 
         char bombPosition = gameBoard[randomLine][randomColumn];
-        if (bombPosition == NULL) {
+        if (bombPosition == '0') {
             bombPosition = '!';
         }
         else {
@@ -29,7 +36,7 @@ void generateBombs(char *gameBoard[]) {
                 int randomColumn = rand() % 17;
 
                 char bombPosition = gameBoard[randomLine][randomColumn];
-            } while (bombPosition != NULL);
+            } while (bombPosition == '0');
 
             bombPosition = '!';
         }
@@ -43,11 +50,27 @@ void checkSurroundings(char position) {
     }
 }
 
-int gameWinner() {}
+//int gameWinner() {}
 
 int gameLoss(char position) {
     if (position == '!') {
         return 1;
     }
     return 0;
+}
+
+void printGameBoard(char gameBoard[16][16]) {
+    printf("   ");
+    for (int i = 0; i < 16; i++) {
+        printf("| %d ", i);
+    }
+    printf("\n");
+
+    for(int i = 0; i < 16; i++) {
+        printf(" %d ", i);
+        for (int j = 0; j < 16; j++) {
+            printf("| %c ", gameBoard[i][j]);
+        }
+        printf("\n");
+    }
 }
